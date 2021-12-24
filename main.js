8d2775aac6d63056179be45b45499657ad140567
Ananth Murugan G K <ananthmgk4002@gmail.com>
2021-12-24 22:49:12 +0530

    Bug cleared by changing string to intiger

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var minutes = 0;
 var seconds = 0;
 var centiseconds = 0;
 var timerCount;
-var stopwatchtimer = document.querySelector('.stopwatch');
 
 function start() {
     let btn = document.getElementById('btn-start');
@@ -20,36 +19,39 @@ function start() {
 }
 
 function startCount() {
-    function count() {
-        centiseconds++;
-        if (centiseconds === 100) {
-          centiseconds = 0;
-          seconds++;
-          if (seconds === 60) {
-            seconds = 0;
-            minutes++;
-            if (minutes === 60) {
-              minutes = 0;
-            }
-            updateMinutes();  
-          }
-          updateSeconds();
-        }
-        updateCentiseconds();
-      }
-      function updateCentiseconds() {
-        document.getElementById('centiseconds').innerHTML = getTimeStr(centiseconds);
-      }
-      function updateSeconds() {
-        document.getElementById('seconds').innerHTML = getTimeStr(seconds);
-      }
-      function updateMinutes() {
-        document.getElementById('minutes').innerHTML = getTimeStr(minutes);
-      }
-      function getTimeStr(t) {
-        return t < 10 ? `0${t}` : `${t}`;
-      }
+    count();
     timerCount = setInterval(count, 10);
+}
+
+function count() {
+  centiseconds++;
+  if (centiseconds === 100) {
+    centiseconds = 0;
+    seconds++;
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes === 60) {
+        minutes = 0;
+      }
+      updateMinutes();  
+    }
+    updateSeconds();
+  }
+  updateCentiseconds();
+}
+
+function updateCentiseconds() {
+  document.getElementById('centiseconds').innerHTML = getTimeStr(centiseconds);
+}
+function updateSeconds() {
+  document.getElementById('seconds').innerHTML = getTimeStr(seconds);
+}
+function updateMinutes() {
+  document.getElementById('minutes').innerHTML = getTimeStr(minutes);
+}
+function getTimeStr(t) {
+  return t < 10 ? `0${t}` : `${t}`;
 }
 
 function stopCount() {
@@ -59,9 +61,11 @@ function stopCount() {
 }
 
 function reset() {
-    minutes = '00';
-    seconds = '00';
-    centiseconds = '00';
-    location.reload();
-    document.getElementById('stopwatch').innerHTML = minutes + ':' + seconds + ':' + centiseconds;
-}
+    minutes = 0;
+    seconds = 0;
+    centiseconds = 0;
+    
+    updateCentiseconds();
+    updateSeconds();
+    updateMinutes();
+} 
